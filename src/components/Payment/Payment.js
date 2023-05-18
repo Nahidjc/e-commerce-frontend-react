fb427e5e6e0b6c0abbec1b3252a79455860b7c03
Nahid Hasan <nahidjc80@gmail.com>
2023-05-18 11:04:14 +0600

    ssl updated

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,11 +1,11 @@
 import Button from '@mui/material/Button';
 import axios from 'axios';
 import React, { useState } from 'react';
+import { api } from '../../configs';
 const Payment = () => {
 
     const [name, setName] = useState("Nahid");
     const [credit, setCredit] = useState(1000);
-    // const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -23,10 +23,9 @@ const Payment = () => {
             },
             data
         };
-        axios.get('http://localhost:8000/payment/init', config)
+        axios.get(`${api}/payment/init`, config)
             .then(response => {
                 window.location.href = response.data.GatewayPageURL;
-                // redirect(response.data.GatewayPageURL);
             })
             .catch(error => {
                 console.error(error);
@@ -67,7 +66,6 @@ const Payment = () => {
                         >
                             Checkout
                         </Button>
-                        {/* <button onClick={handleSubmit}>Checkout</button> */}
                     </div>
 
                 </form>
